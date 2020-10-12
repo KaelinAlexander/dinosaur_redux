57f7c7225a644fb3029aafeeecae9f292f81299b
KaelinAlexander <kaelin.alexander@gmail.com>
2020-10-12 15:09:29 -0600

    initial wiring for fetch from api

--- dinosaur-client/src/components/Home.js
+++ dinosaur-client/src/components/Home.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { fetchDinosaurs } from '../actions/fetchDinosaurs'
 
 class Home extends Component {
+
+    handleOnClick() {
+        this.props.fetchDinosaurs()
+    }
+
     render() {
+    const dinosaurs = this.props.dinosaurs.map(dino => <li key={dino.id}>{dino.genus}</li>)
         return (
             <div>
-                Testing Index
+                <button onClick={(event) => this.handleOnClick(event)} />
+                { dinosaurs }
             </div>
-        )
+        );
     }
+};
+
+function mapDispatchToProps(dispatch) {
+    return { fetchDinosaurs: () => dispatch(fetchDinosaurs())}
 }
 
-export default Home
+function mapStateToProps(state) {
+    return {dinosaurs: state.dinosaurs}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
--- dinosaur-client/src/index.js
+++ dinosaur-client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
