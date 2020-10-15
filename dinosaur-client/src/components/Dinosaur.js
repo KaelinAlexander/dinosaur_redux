1631d824a37a809a8c1e547441be002b0656100d
KaelinAlexander <kaelin.alexander@gmail.com>
2020-10-15 12:10:17 -0600

    adds initial tabbed browsing to view page

--- dinosaur-client/src/components/Dinosaur.js
+++ dinosaur-client/src/components/Dinosaur.js
@@ -1,21 +1,92 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { deleteDinosaur } from '../actions/dinosaurActions'
 
 class Dinosaur extends Component {
+
+    state = {
+        basicsShow: "",
+        historyShow: "is-hidden",
+        cladeShow: "is-hidden"
+    }
 
     handleClick = () => {
         this.props.deleteDinosaur(this.props.dinosaur.id)
         this.props.history.push('/')
     }
 
+    handleBasics = () => {
+        this.setState({
+            basicsShow: "",
+            historyShow: "is-hidden",
+            cladeShow: "is-hidden"
+        })
+    }
+
+    handleHistory = () => {
+        this.setState({
+            basicsShow: "is-hidden",
+            historyShow: "",
+            cladeShow: "is-hidden"
+        })
+    }
+
+    handleClade = () => {
+        this.setState({
+            basicsShow: "is-hidden",
+            historyShow: "is-hidden",
+            cladeShow: ""
+        })
+    }
+
     render() {
+        if (this.props.dinosaur) {
         return(
         <div>
-            It worked! Let's find out all about {this.props.dinosaur.genus}!
+            <div className="columns">
+                <div className="column">
+                    <div className="tabs is-centered is-boxed">
+                        <ul>
+                            <li>
+                            <Link onClick={this.handleBasics}>Basics</Link>  
+                            </li>
+                            <li>
+                            <Link onClick={this.handleHistory}>History</Link>  
+                            </li>
+                            <li>
+                            <Link onClick={this.handleClade}>Clade</Link>  
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div id="tab-content">
+
+                <div id="dino-basics" className={this.state.basicsShow}>
+                It worked! Let's find out all about {this.props.dinosaur.genus}!
+                </div>
+
+                <div id="dino-history" className={this.state.historyShow}>
+                This is the history page.
+                </div>
+
+                <div id="dino-clade" className={this.state.cladeShow}>
+                This is the clade page.
+                </div>
+
+            </div>
+            
             <button onClick={this.handleClick}>Delete Dinosaur</button>
+
         </div>
         )
+        } else {
+            return (
+                <h1>Content loading...</h1>
+            )
+        }
     }
 }
 
